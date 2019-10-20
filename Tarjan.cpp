c562d39b344e755480c6f5b5c3dd704d106fffe8
viento <3576993219@qq.com>
2019-10-20 20:00:22 +0800

    begin

---
+++ Tarjan.cpp
@@ -0,0 +1,31 @@
+
+/*
+ * poj1236,poj3177,poj3164
+ */
+
+int dfn[maxn],low[maxn],scc[maxn], stk[maxn],index=0,sccnum=0,top=0;\
+
+void tarjan(int root){
+    if(dfn(root)) return ;
+    dfn[root] = low[root]= ++index;
+    stk[++top] = root;
+    for (int i = head[root]; ~i ; i=e[i].ne) {
+        int v = e[i].to;
+        if(!dfn[v]){
+            tarjan(v);
+            low[root] = min(low[root],low[v]);
+        }else if(!scc[v]){
+            low[root] = min(low[root],dfn[v]);
+        }
+    }
+
+    if(low[root]==dfn[root]){
+        sccnum++;
+        for (;;){
+            int x = stk[top--];
+            scc[x]= sccnum;
+            if(x == root) break;
+        }
+    }
+}
+
